f9ca0322638ea34b82243d76a7647f9679853130
pRemant <paudelremant45@gmail.com>
2023-07-22 01:12:53 +0545

    applied css styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,7 +15,7 @@ const NavBar = () => {
         <Navbar.Brand className="fs-4" as={NavLink} to="/">Online<span>Store</span></Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav className="ms-auto fs-5 my-2 my-lg-0">
+          <Nav className="ms-auto my-2 my-lg-0">
             <Nav.Link as={NavLink} to="/">Home</Nav.Link>
             <Nav.Link className="px-4" as={NavLink} to="/search">Search</Nav.Link>
             <Nav.Link as={NavLink} to="/cart/item"><i className="bi bi-cart-dash"></i>(<span>{items.length}</span>)</Nav.Link>

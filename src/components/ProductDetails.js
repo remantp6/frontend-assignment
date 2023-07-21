f98a7b420d778c839f368ebb6efdb60611ad69ed
pRemant <paudelremant45@gmail.com>
2023-07-21 15:28:13 +0545

    created ProductDetails component

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductDetails = () => {
+  return (
+    <>
+      ProductDetails page
+    </>
+  )
+}
+
+export default ProductDetails
---
+++ src/pages/ProductDetailsPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+import ProductDetails from '../components/ProductDetails'
+import Footer from '../components/Footer'
+
+const ProductDetailsPage = () => {
+  return (
+    <>
+      <NavBar/>
+      <ProductDetails/>
+      <Footer/>
+    </>
+  )
+}
+
+export default ProductDetailsPage

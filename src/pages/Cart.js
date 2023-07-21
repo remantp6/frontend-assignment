16f3a9807b9ee48a3a89b7d052378165d5c6cab6
pRemant <paudelremant45@gmail.com>
2023-07-22 01:04:56 +0545

    Footer component added

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import CartItem from '../components/CartItem'
+import Footer from '../components/Footer'
 
 const Cart = () => {
   return (
     <>
     <NavBar/>
     <CartItem/>
+    <Footer/>
     </>
   )
 }

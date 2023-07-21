5c2b7726c5039dda7937fd7996570e5801456d76
pRemant <paudelremant45@gmail.com>
2023-07-22 01:36:08 +0545

    added Footer component

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import SearchProduct from '../components/SearchProduct'
+import Footer from '../components/Footer'
 
 const Search = () => {
   return (
     <>
     <NavBar/>
     <SearchProduct/>
+    <Footer/>
     </>
   )
 }

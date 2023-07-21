45b240c19f2a62cf963c2c6b4b5ce199a450b019
pRemant <paudelremant45@gmail.com>
2023-07-21 14:24:00 +0545

    created Footer component and applied css styling

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import "../assets/css/Footer.css"
+import { Col, Container, Row } from "react-bootstrap";
+
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer-content">
+        <Container>
+          <Row>
+            <Col sm={4} className="px-5 px-sm-2">
+              <h6 className='pb-1'>Information</h6>
+              <li>About Us</li>
+              <li>Privacy Policy</li>
+              <li>Returns Policy</li>
+              <li className="mb-0">Shipping Policy</li>
+            </Col>
+            <Col sm={4} className="px-5">
+              <h6 className='pb-1'>Account</h6>
+              <li>Dashboard</li>
+              <li>My Orders</li>
+              <li>My Wishlist</li>
+              <li className="mb-0">Account</li>
+            </Col>
+            <Col sm={4} className="px-5">
+              <h6 className='pb-1'>Categories</h6>
+              <li> Clothing</li>
+              <li>Bags</li>
+              <li>Jewelery</li>
+              <li className=" mb-0">Electronics</li>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  )
+}
+
+export default Footer

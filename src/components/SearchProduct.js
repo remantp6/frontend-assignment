7f1a42f40492c0a5ba4e141f0319861aa37da121
pRemant <paudelremant45@gmail.com>
2023-07-21 15:09:16 +0545

    created SearchProduct component and applied css styling

---
+++ src/components/SearchProduct.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "../assets/css/SearchProduct.css";
+import { useQuery } from "react-query";
+import { fetchProducts } from "../services/FetchApi";
+import { Container } from "react-bootstrap";
+
+const SearchProduct = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const { data } = useQuery("products", fetchProducts);
+  const [searchResults, setSearchResults] = useState([]);
+
+  //handleSubmit function to handle the form submission
+  const handleSubmit = (e) => {
+    //prevent default behavior of form submission
+    e.preventDefault();
+
+    //data array is filtered based on whether the product's title or category includes the search term
+    const results = data.filter(
+      (product) =>
+        product.category.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        product.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    //updating the searchResults state variable with the filtered results.
+    setSearchResults(results);
+
+    // resets the searchTerm state variable
+    setSearchTerm("");
+
+    if (results.length === 0) {
+      setSearchResults(null); // Clear any previous search results
+    }
+  };
+
+  return (
+    <>
+      <div className="search-list">
+        <Container>
+          <form onSubmit={handleSubmit} className="pt-5 ms-auto me-auto">
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              placeholder="Search Products..."
+            />
+            <button type="submit">Submit</button>
+          </form>
+          {searchResults !== null ? (
+            <div className="search-results me-auto ms-auto mt-5">
+              <div className="search-items-wrapper">
+                {searchResults.map((product) => (
+                  <div className="search-item" lg={3} key={product.id}>
+                    <img src={product.image} alt="img" className="w-100" />
+                    <p className="mb-0 pt-2">{product.price}</p>
+                    <p className="mb-0">{product.category}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <div className="no-results-message">
+              <p className="fs-5 my-5">No products in the Cart.</p>
+            </div>
+          )}
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default SearchProduct;
